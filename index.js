6f0ae7433e574d12bdeffb2a851f0b3d690fbe52
Amaurys Almanzar <amaurys.almanzar@wework.com>
2019-05-11 02:40:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
   return delete object.key
 }
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  return delete object.key 
+}
