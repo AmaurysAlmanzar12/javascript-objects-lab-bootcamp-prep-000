9f1e7a37bc92555aa59577b9a12203362b8e7de5
Amaurys Almanzar <amaurys.almanzar@wework.com>
2019-05-11 02:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
-  return object 
-  
+  return object
+
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
